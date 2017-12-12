59db2034b179a5aadddb6179af200a1140aa1ade
James Casey <jameslcasey@gmail.com>
2017-12-11 22:49:08 -0600

    made a release.

--- Borderlands2Guns/Program.cs
+++ Borderlands2Guns/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
 using Borderlands2Guns.Models;
+using System.Text.RegularExpressions;
 
 namespace Borderlands2Guns
 {
@@ -42,5 +43,75 @@ namespace Borderlands2Guns
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
+
+
+
+        //public static void Main()
+        //{
+        //    Console.WriteLine("Enter any text, followed by <Enter>:\n");
+        //    String s = Console.ReadLine();
+        //    ShowWords(s);
+        //    Console.Write("\nPress any key to continue... ");
+        //    Console.ReadKey();
+        //}
+
+        //private static void ShowWords(String s)
+        //{
+        //    String pattern = @"\w+";
+        //    var matches = Regex.Matches(s, pattern);
+        //    if (matches.Count == 0)
+        //    {
+        //        Console.WriteLine("\nNo words were identified in your input.");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine($"\nThere are {matches.Count} words in your string:");
+        //        for (int ctr = 0; ctr < matches.Count; ctr++)
+        //        {
+        //            Console.WriteLine($"   #{ctr,2}: '{matches[ctr].Value}' at position {matches[ctr].Index}");
+        //        }
+        //    }
+        //}
+
+
+
+
+
+
     }
 }
+
+
+
+//namespace Applications.ConsoleApps
+//{
+//    public class ConsoleParser
+//    {
+//        public static void Main()
+//        {
+//            Console.WriteLine("Enter any text, followed by <Enter>:\n");
+//            String s = Console.ReadLine();
+//            ShowWords(s);
+//            Console.Write("\nPress any key to continue... ");
+//            Console.ReadKey();
+//        }
+
+//        private static void ShowWords(String s)
+//        {
+//            String pattern = @"\w+";
+//            var matches = Regex.Matches(s, pattern);
+//            if (matches.Count == 0)
+//            {
+//                Console.WriteLine("\nNo words were identified in your input.");
+//            }
+//            else
+//            {
+//                Console.WriteLine($"\nThere are {matches.Count} words in your string:");
+//                for (int ctr = 0; ctr < matches.Count; ctr++)
+//                {
+//                    Console.WriteLine($"   #{ctr,2}: '{matches[ctr].Value}' at position {matches[ctr].Index}");
+//                }
+//            }
+//        }
+//    }
+//}
---
+++ Borderlands2Guns/bin/Release/PublishOutput/wwwroot/js/site.js
@@ -0,0 +1,51 @@
+﻿// Write your JavaScript code.
+
+$(document).ready(function () {
+
+    var resultsDiv = $("#searchResults");
+
+    $("#gunname").on("keyup", function () {
+
+        resultsDiv.empty();
+
+        var term = $(this).val();
+
+        $.get("GunNameSearch?ss=" + term, function (data, status) {
+            var json = $.parseJSON(data);
+            for (var i = 0; i < json.length; i++) {
+                resultsDiv.append("<div class='row'> <div class='col-md-1'>" + json[i].Level + "</div> <div class='col-md-4'>" + json[i].Name + "</div> <div class='col-md-2'>" + json[i].DamageOnTarget + "</div> <div class='col-md-2'>" + json[i].AllTypesDamageOnTargetRank + "</div> </div>");
+            }
+
+        });
+    });
+
+    $("#damage, #accuracy,  #firerate, #reloadspeed, #magazinesize, #elementalDamagePerSecond, #chance").on("keyup", function () {
+
+        var damage = $("#damage").val();
+        var accuracy = $("#accuracy").val();
+        var firerate = $("#firerate").val();
+        var reloadspeed = $("#reloadspeed").val();
+        var magazinesize = $("#magazinesize").val();
+        var elementalDamagePerSecond = $("#elementalDamagePerSecond").val();
+        var chance = $("#chance").val();
+
+        $.get("GunCalcs?damage=" + damage + "&accuracy=" + accuracy + "&firerate=" + firerate + "&reloadspeed=" + reloadspeed + "&magazinesize=" + magazinesize + "&elementalDamagePerSecond=" + elementalDamagePerSecond + "&chance=" + chance, function (data, status) {
+
+            var json = $.parseJSON(data);
+            $("#damageTimesFireRate").val(json["DamageTimesFireRate"]);
+            $("#damageTimesFireRateTimesMagazineSize").val(json["DamageTimesFireRateTimesMagazineSize"]);
+            $("#damageTimesFireRateTimesMagazineSizePerReloadSpeed").val(json["DamageTimesFireRateTimesMagazineSizePerReloadSpeed"]);
+            $("#damageOnTarget").val(json["DamageOnTarget"]);
+            $("#elementalDamagePerSecondTimesChance").val(json["ElementalDamagePerSecondTimesChance"]);
+            $("#elementalDamageOnTargetTimesDamagePerSecondTimesChance").val(json["ElementalDamageOnTargetTimesDamagePerSecondTimesChance"]);
+
+
+        });
+
+    });
+
+    $("#indexpage").DataTable();
+
+
+
+});
